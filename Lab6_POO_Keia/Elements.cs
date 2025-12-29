5fdee7f4325e6af19b7c1bf962edce9150ef3080
Patrice Waechter-Ebling <patdev.qc.ca@outlook.com>
2025-12-29 15:50:40 -0500

    Add files via upload

---
+++ Lab6_POO_Keia/Elements.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KeiaLab6
+{
+    /*  \brief Classe elements
+     *  gere les proprietes des items 
+     */
+    class Elements 
+    {
+        int id; //id= nomgre relatif a la position
+        int Quantite;// nombre de pieces requise par assemblage
+        string name; // nom de la piece
+        string description; //text user-friendly pour decrire la piece 
+
+        public Elements(int id, int quantite, string name, string description)//constructeur 
+        {
+            //initialisation et assignation 
+            this.id = id;
+            Quantite = quantite;
+            this.name = name;
+            this.description = description;
+        }
+
+        public int GetId() { return this.id; }//retourne la position
+        public int GetQuantite() {return this.Quantite; }//retourne la quantité
+        public string GetName() { return this.name; }//retourne le nom
+        public string GetDescription() { return this.description; }//retourne la description
+    }
+}
---
+++ Lab6_POO_Keia/README.md
@@ -0,0 +1,17 @@
+# Equipe2
+Lache pas la patate Louis t capable t pas une tomate
+sudo
+mkdir /home/patrice/hddvirt/
+mkdir /home/patrice/hddvirt/PERSONNEL/
+mkdir /home/patrice/hddvirt/PERSONNEL/VOYAGES			
+mkdir /home/patrice/hddvirt/FORMATION/				
+mkdir /home/patrice/hddvirt/FORMATION/INFORMATIQUE			
+mkdir /home/patrice/hddvirt/FORMATION/INFORMATIQUE/STI		
+mkdir /home/patrice/hddvirt/FORMATION/MATHS	/		
+mkdir /home/patrice/hddvirt/FORMATION/MATHS	/PRATIQUE		
+mkdir /home/patrice/hddvirt/FORMATION/MATHS	/DIFF		
+mkdir /home/patrice/hddvirt/FORMATION/LANGUES/			
+mkdir /home/patrice/hddvirt/FORMATION/LANGUES/ANGLAIS/		
+mkdir /home/patrice/hddvirt/FORMATION/LANGUES/ANGLAIS/COURS
+mkdir /home/patrice/hddvirt/FORMATION/LANGUES/ANGLAIS/TP
+mkdir /home/patrice/hddvirt/FORMATION/LANGUES/AUTRES	
